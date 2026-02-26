fbea428564fd899417cb5b68dd9f029ad8758113
Aviral Verma <aviralv@micron.com>
2026-02-26 11:10:49 +0530

    Windows installer support removed.

--- windows-installer/start-app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Wrapper script to start Auto Stretch with properly quoted paths
-REM NSSM calls this script, which then calls Python
-
-cd /d "C:\Program Files\AutoStretch"
-"C:\Program Files\AutoStretch\python\python.exe" "C:\Program Files\AutoStretch\app.py"
